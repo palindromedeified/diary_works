8c5270ce2597cf985af5d8fb5b5b863d524c355e
Пользователь <holanpolain090@gmail.com>
2024-03-21 16:43:45 +0300

    spec message

--- data/users.py
+++ data/users.py
@@ -2,13 +2,13 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import UserMixin
 from .db_session import SqlAlchemyBase
 
 
 # ##### Это надо отправить -- начало{
 # Модель Марсиане
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
